7cc1d7cab671bc23d4a982037abbe6240807512a
xime.zuber <xzuberbuhler@itba.edu.ar>
2018-11-29 01:21:14 -0300

    Add matrix functions

--- Utilities/matrix.c
+++ Utilities/matrix.c
@@ -1,46 +1,211 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "matrix.h"
 
 
 
 matrix newMatrix() {
-	return calloc(1, sizeof(matrix));
+	return calloc(1, sizeof(matrixT));
 }
 
 void freeMatrix(matrix mat){
 	if(mat == NULL)
 		return;
 
-	if(mat->type != NULL){
-		free(mat->type);
-	}
 	if(mat->mtrx != NULL){
 		free(mat->mtrx);
 	}
 	free(mat);
 }
 
-matrix createMatrix(size_t w, size_t h, void *t) {
+matrix createMatrix(size_t w, size_t h, int t) {
 	matrix mat = newMatrix();
 
-	if(t == NULL)
+	switch(t) {
+		case INTEGER_TYPE: mat->type = INTEGER_TYPE;
+		mat->mtrx = calloc(w*h,sizeof(int));
+		break;
+		case FLOAT_TYPE: mat->type = FLOAT_TYPE;
+		mat->mtrx = calloc(w*h,sizeof(float));
+		break;
+		case DOUBLE_TYPE: mat->type = DOUBLE_TYPE;
+		mat->mtrx = calloc(w*h,sizeof(double));
+		break;
+		case CHAR_TYPE: mat->type = CHAR_TYPE;
+		mat->mtrx = calloc(w*h,sizeof(char));
+		break;
+		case STRING_TYPE: mat->type = STRING_TYPE;
+		mat->mtrx = calloc(w*h,sizeof(char *));
+		break;
+		default: freeMatrix(mat);return NULL;
+	}
+	
+	if (mat->mtrx == NULL){
+		freeMatrix(mat);
 		return NULL;
+	}
 
-	mat->type = calloc(1, sizeof(t));
-	if(mat->type == NULL)
-		return NULL;
-	memcpy(mat->type,t,sizeof(t));
-
-
-	int matSize = w*h*sizeof(t);
 	mat->width = w;
 	mat->height = h;
-	mat->mtrx = calloc(1, matSize);
-	if(mat->mtrx == NULL){
-		free(mat->type);
+
+
+	return mat;
+}
+
+void addValue(int row, int col, int value, matrix mat){
+	int * m = mat->mtrx;
+	*(m + (row*mat->width + col))  = value;
+}
+
+
+  
+matrix sumMatrix( matrix a, matrix b) {
+	
+	if(b->type != INTEGER_TYPE && b->type != DOUBLE_TYPE && b->type != FLOAT_TYPE && a->type != INTEGER_TYPE && a->type != DOUBLE_TYPE && a->type != FLOAT_TYPE) { 
 		return NULL;
+	}
+
+	if(a->width != b->width || a->height != b->height) {
+		return NULL;
+	}
+ 
+	int i,k;
+	matrix mat;
+	
+	if(a->type >= b->type){
+		mat = createMatrix(a->width, a->height, a->type);
+	}else{
+		mat = createMatrix(a->width, a->height, b->type);
+	}
+
+	int * m = a->mtrx;
+	int * n = b->mtrx;
+
+
+	for(i = 0; i < a->width; i++) {
+		for(k = 0; k < a->height; k++) {
+			addValue(i, k,  m[k*a->width + i] + n[k*b->width + i], mat);
+		}
 	}
 
 	return mat;
 }
+
+matrix subMatrix( matrix a, matrix b) {
+	
+	if(b->type != INTEGER_TYPE && b->type != DOUBLE_TYPE && b->type != FLOAT_TYPE && a->type != INTEGER_TYPE && a->type != DOUBLE_TYPE && a->type != FLOAT_TYPE) { 
+		return NULL;
+	}
+
+	if(a->width != b->width || a->height != b->height) {
+		return NULL;
+	}
+ 
+	int i,k;
+	matrix mat;
+	
+	if(a->type >= b->type){
+		mat = createMatrix(a->width, a->height, a->type);
+	}else{
+		mat = createMatrix(a->width, a->height, b->type);
+	}
+
+	int * m = a->mtrx;
+	int * n = b->mtrx;
+
+
+	for(i = 0; i < a->width; i++) {
+		for(k = 0; k < a->height; k++) {
+			addValue(i, k,  m[k*a->width + i] - n[k*b->width + i], mat);
+		}
+	}
+
+	return mat;
+}
+
+
+matrix multMatrixEsc( matrix a, double e) {
+	
+	if(a->type != INTEGER_TYPE && a->type != DOUBLE_TYPE && a->type != FLOAT_TYPE) { 
+		return NULL;
+	}
+
+
+	int i,k;
+	matrix mat;
+	
+	mat = createMatrix(a->width, a->height, a->type);
+	
+
+	int * m = a->mtrx;
+
+
+	for(i = 0; i < a->width; i++) {
+		for(k = 0; k < a->height; k++) {
+			addValue(i, k,  m[k*a->width + i] * e, mat);
+		}
+	}
+
+
+	return mat;
+}
+
+
+matrix mulMatrix( matrix a, matrix b) {
+	
+	if(b->type != INTEGER_TYPE && b->type != DOUBLE_TYPE && b->type != FLOAT_TYPE && a->type != INTEGER_TYPE && a->type != DOUBLE_TYPE && a->type != FLOAT_TYPE) { 
+		return NULL;
+	}
+
+	if(a->width != a->height) {
+		return NULL;
+	}
+ 
+	
+	matrix mat;
+	
+	if(a->type >= b->type){
+		mat = createMatrix(a->width, a->height, a->type);
+	}else{
+		mat = createMatrix(a->width, a->height, b->type);
+	}
+
+	int * m = a->mtrx;
+	int * n = b->mtrx;
+	int c,d,k,sum = 0;
+
+	for (c = 0; c < a->height; c++) {
+    	for (d = 0; d < b->width; d++) {
+    		for (k = 0; k < a->width; k++) {
+          		sum = sum + m[c*a->width + k] * n[k*b->width + d];
+        	}
+        	addValue(c, d,  sum, mat);
+    	    sum = 0;
+      	}
+    }
+
+	return mat;
+}
+
+matrix transMatrix(matrix a){
+
+	matrix mat;
+	
+	mat = createMatrix(a->width, a->height, a->type);
+
+	int * m = a->mtrx;
+
+	for(i = 0; i < a->width; i++) {
+		for(k = 0; k < a->height; k++) {
+			addValue(i, k,  m[k*a->width + i] * e, mat);
+		}
+	}
+	
+	return mat;
+
+}
+
+
+
+
